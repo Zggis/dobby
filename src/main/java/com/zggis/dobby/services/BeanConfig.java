3e02ae498561c2182de4dd5a1906a70020c3d90c
Sean Watts <swatts@alumni.stevens.edu>
2022-11-06 10:13:46 -0500

    Added version printout

--- src/main/java/com/zggis/dobby/services/BeanConfig.java
+++ src/main/java/com/zggis/dobby/services/BeanConfig.java
@@ -1,30 +1,47 @@
 package com.zggis.dobby.services;
 
+import javax.annotation.PostConstruct;
+
+import com.zggis.dobby.batch.ConsoleColor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
 
-	@Value("${linux}")
-	private boolean isLinux;
+    private static final Logger logger = LoggerFactory.getLogger(BeanConfig.class);
 
-	@Bean
-	public DoviProcessBuilder doviProcessBuilder() {
-		if (isLinux) {
-			return new DoviLinuxProcessBuilderImpl();
-		} else {
-			return new DoviWindowsProcessBuilderImpl();
-		}
-	}
+    @Value("${linux}")
+    private boolean isLinux;
 
-	@Bean
-	public MediaService mediaService() {
-		if (isLinux) {
-			return new LinuxMediaServiceImpl();
-		} else {
-			return new WindowMediaServiceImpl();
-		}
-	}
+    @Autowired
+    private BuildProperties buildProperties;
+
+    @PostConstruct
+    public void init() {
+        logger.info(ConsoleColor.CYAN.value + "Running Dobby v{}" + ConsoleColor.NONE.value, buildProperties.getVersion());
+    }
+
+    @Bean
+    public DoviProcessBuilder doviProcessBuilder() {
+        if (isLinux) {
+            return new DoviLinuxProcessBuilderImpl();
+        } else {
+            return new DoviWindowsProcessBuilderImpl();
+        }
+    }
+
+    @Bean
+    public MediaService mediaService() {
+        if (isLinux) {
+            return new LinuxMediaServiceImpl();
+        } else {
+            return new WindowMediaServiceImpl();
+        }
+    }
 }
